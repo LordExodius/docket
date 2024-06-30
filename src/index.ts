3bfebd4c2569feb80d077638009119641904d105
LordExodius <oscar45697@gmail.com>
2024-06-30 04:01:36 -0400

    got rollup working and added highlight.js

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
-import { marked } from 'marked';
+import { Marked } from 'marked'
+import DOMPurify from 'dompurify'
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js/lib/core'
+// Import supported code languages (for size purposes)
+import c from 'highlight.js/lib/languages/c'
+import cpp from 'highlight.js/lib/languages/cpp'
+import java from 'highlight.js/lib/languages/java'
+import javascript from 'highlight.js/lib/languages/javascript'
+import python from 'highlight.js/lib/languages/python'
+import rust from 'highlight.js/lib/languages/rust'
+import typescript from 'highlight.js/lib/languages/typescript'
+
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('rust', rust);
+hljs.registerLanguage('typescript', typescript);
 
 /**
- * Summary: Rerender markdown every 500ms while typing, and also 500ms after no input.
+ * Summary: Rerender markdown every {timeout}ms while typing, and also {timeout}ms after no input.
  */
 
 const renderMarkdown = () => {
-    const inputText = document.getElementById("textInput")!.innerHTML || ""
-    document.getElementById("mdRender")!.innerHTML = marked.parse(inputText) as keyof typeof String
+    const inputText = (<HTMLInputElement>document.getElementById("textInput"))!.value || ""
+    const cleanText = DOMPurify.sanitize(inputText)
+    // console.log(cleanText)
+    const marked = new Marked(
+        markedHighlight({
+          langPrefix: 'hljs language-',
+          highlight(code, lang, info) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+            return hljs.highlight(code, { language }).value
+          }
+        })
+    )
+    document.getElementById("mdRender")!.innerHTML = marked.parse(cleanText) as keyof typeof String
+    
+    // code block background highlight
+    const codeBlocks = Array.from(document.getElementsByTagName("code"))
+    codeBlocks.map(code => {
+        (<HTMLElement>code.parentElement).style.backgroundColor = "#f6f8fa"
+    })
 }
 
 interface LastExecuted {
@@ -14,9 +50,9 @@ interface LastExecuted {
     msSinceLastUpdate: number
 }
 
-const timeout = 500;
+const timeout = 0;
 
-// Debounce rendering to rerender 500ms after no input
+// Debounce rendering to rerender after no input detected for {timeout}ms
 const debounce = (lastExecuted: LastExecuted) => {
     if (Date.now() - lastExecuted.msSinceLastInput > timeout) {
         renderMarkdown()
@@ -25,7 +61,6 @@ const debounce = (lastExecuted: LastExecuted) => {
 
 const handleInput = (lastExecuted: LastExecuted) => {
     let currTime = Date.now()
-
     // debounce
     lastExecuted.msSinceLastInput = currTime
     setTimeout(debounce, timeout, lastExecuted)
@@ -38,10 +73,23 @@ const handleInput = (lastExecuted: LastExecuted) => {
 }
 
 // Add event listener to the editable side
-document.getElementById("textInput")?.addEventListener("input", 
+const textInput = <HTMLInputElement>document.getElementById("textInput")
+
+textInput.addEventListener("input", 
     handleInput.bind(undefined, {
         msSinceLastInput: Date.now(),
         msSinceLastUpdate: Date.now()
     }))
 
-export = {}
+textInput.addEventListener("keydown", 
+    (e) => {
+        if (e.key === "Tab") {
+            e.preventDefault()
+            textInput.setRangeText(
+                "\t",
+                textInput.selectionStart || 0,
+                textInput.selectionEnd || 0,
+                "end"
+            )
+        }
+})
