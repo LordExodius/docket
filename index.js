b5c7626108041115de4cc7d4bc320bf16d705429
LordExodius <oscar45697@gmail.com>
2024-06-30 04:01:36 -0400

    got rollup working and added highlight.js

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
-"use strict";
-
-// src/index.ts
-var import_marked = require("marked");
-var renderMarkdown = () => {
-  const inputText = document.getElementById("textInput").innerHTML || "";
-  document.getElementById("mdRender").innerHTML = import_marked.marked.parse(inputText);
+import { Marked } from 'marked';
+import DOMPurify from 'dompurify';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js/lib/core';
+// Import supported code languages (for size purposes)
+import c from 'highlight.js/lib/languages/c';
+import cpp from 'highlight.js/lib/languages/cpp';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import rust from 'highlight.js/lib/languages/rust';
+import typescript from 'highlight.js/lib/languages/typescript';
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('rust', rust);
+hljs.registerLanguage('typescript', typescript);
+/**
+ * Summary: Rerender markdown every {timeout}ms while typing, and also {timeout}ms after no input.
+ */
+const renderMarkdown = () => {
+    const inputText = document.getElementById("textInput").value || "";
+    const cleanText = DOMPurify.sanitize(inputText);
+    // console.log(cleanText)
+    const marked = new Marked(markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang, info) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        }
+    }));
+    document.getElementById("mdRender").innerHTML = marked.parse(cleanText);
+    // code block background highlight
+    const codeBlocks = Array.from(document.getElementsByTagName("code"));
+    codeBlocks.map(code => {
+        code.parentElement.style.backgroundColor = "#f6f8fa";
+    });
 };
-var timeout = 500;
-var debounce = (lastExecuted) => {
-  if (Date.now() - lastExecuted.msSinceLastInput > timeout) {
-    renderMarkdown();
-  }
+const timeout = 0;
+// Debounce rendering to rerender after no input detected for {timeout}ms
+const debounce = (lastExecuted) => {
+    if (Date.now() - lastExecuted.msSinceLastInput > timeout) {
+        renderMarkdown();
+    }
 };
-var handleInput = (lastExecuted) => {
-  let currTime = Date.now();
-  lastExecuted.msSinceLastInput = currTime;
-  setTimeout(debounce, timeout, lastExecuted);
-  if (currTime - lastExecuted.msSinceLastUpdate > timeout) {
-    renderMarkdown();
-    lastExecuted.msSinceLastUpdate = currTime;
-  }
+const handleInput = (lastExecuted) => {
+    let currTime = Date.now();
+    // debounce
+    lastExecuted.msSinceLastInput = currTime;
+    setTimeout(debounce, timeout, lastExecuted);
+    // rerender every 500ms while typing
+    if (currTime - lastExecuted.msSinceLastUpdate > timeout) {
+        renderMarkdown();
+        lastExecuted.msSinceLastUpdate = currTime;
+    }
 };
-var _a;
-(_a = document.getElementById("textInput")) == null ? void 0 : _a.addEventListener(
-  "input",
-  handleInput.bind(void 0, {
+// Add event listener to the editable side
+const textInput = document.getElementById("textInput");
+textInput.addEventListener("input", handleInput.bind(undefined, {
     msSinceLastInput: Date.now(),
     msSinceLastUpdate: Date.now()
-  })
-);
-module.exports = {};
+}));
+textInput.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+        e.preventDefault();
+        textInput.setRangeText("\t", textInput.selectionStart || 0, textInput.selectionEnd || 0, "end");
+    }
+});
+//# sourceMappingURL=index.js.map
