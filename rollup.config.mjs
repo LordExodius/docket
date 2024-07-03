4add27b8d4b51c5004712d1a4e9d56a09df57af2
Hunter Chen <hello@hunterchen.ca>
2024-07-03 18:47:44 -0400

    minify bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,17 @@
 // rollup.config.js
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 
 export default {
-  input: 'dist/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'iife',
-    name: 'MyModule'
-  },
-  plugins: [commonjs(), resolve()]
+  input: "dist/index.js",
+  output: [
+    {
+      file: "dist/bundle.js",
+      format: "iife",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [commonjs(), resolve()],
 };
