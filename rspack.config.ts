720f5e0c6d62536cc020e553b481395935830b9c
Oscar Yu <oscar45697@gmail.com>
2025-10-13 06:11:15 -0400

    add license disclosures (#13)
    * add license-webpack-plugin
    * include marked and dompurify

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,32 +1,53 @@
-const path = require('path');
+const path = require("path");
+const LicenseWebpackPlugin =
+  require("license-webpack-plugin").LicenseWebpackPlugin;
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: [/node_modules/],
-        loader: 'builtin:swc-loader',
+        loader: "builtin:swc-loader",
         options: {
           jsc: {
             parser: {
-              syntax: 'typescript',
+              syntax: "typescript",
             },
           },
         },
-        type: 'javascript/auto',
+        type: "javascript/auto",
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   optimization: {
     minimize: true,
   },
+  plugins: [
+    new LicenseWebpackPlugin({
+      outputFilename: "dependency.licenses.txt",
+      handleMissingLicenseText: (packageName: string, licenseType: string) => {
+        console.log("Cannot find license for " + packageName);
+        return "UNKNOWN";
+      },
+      additionalModules: [
+        {
+          name: "dompurify",
+          directory: path.join(__dirname, "node_modules", "dompurify"),
+        },
+        {
+          name: "marked",
+          directory: path.join(__dirname, "node_modules", "marked"),
+        },
+      ],
+    }),
+  ],
 };
