f0ce9472c0b17b5c1691de6202aaf8703a8f7098
LordExodius <oscar45697@gmail.com>
2025-10-13 05:51:43 -0400

    add license-webpack-plugin

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,32 +1,46 @@
-const path = require('path');
+const path = require("path");
+const LicenseWebpackPlugin =
+  require("license-webpack-plugin").LicenseWebpackPlugin;
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: [/node_modules/],
-        loader: 'builtin:swc-loader',
+        loader: "builtin:swc-loader",
         options: {
           jsc: {
             parser: {
-              syntax: 'typescript',
+              syntax: "typescript",
             },
           },
         },
-        type: 'javascript/auto',
+        type: "javascript/auto",
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   optimization: {
     minimize: true,
   },
+  plugins: [
+    new LicenseWebpackPlugin({
+      stats: {
+        warnings: true,
+        errors: true,
+      },
+      handleMissingLicenseText: (packageName: string, licenseType: string) => {
+        console.log("Cannot find license for " + packageName);
+        return "UNKNOWN";
+      },
+    }),
+  ],
 };
